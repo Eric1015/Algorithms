7214b486a7eca1311d6191a8cbd083366313f280
love1 <love1dbsb77@hotmail.com>
2018-04-15 17:25:35 -0700

    Complete the implementation of selectionSort.cpp

--- Algorithms/Selection_Sort/selectionSort.cpp
+++ Algorithms/Selection_Sort/selectionSort.cpp
@@ -11,10 +11,36 @@ selectionSort<T>::selectionSort(std::vector<T> &arr, bool isAsc){
         desSort(arr);
 }
 
-template<class T>
-void selectionSort<T>::ascSort(std::vector<T> &arr){
+template <class T>
+void selectionSort<T>::ascSort(std::vector<T> &arr)
+{
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        for (size_t j = i; j < arr.size(); j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                T temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
 }
 
-template<class T>
-void selectionSort<T>::desSort(std::vector<T> &arr){
+template <class T>
+void selectionSort<T>::desSort(std::vector<T> &arr)
+{
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        for (size_t j = i; j < arr.size(); j++)
+        {
+            if (arr[j] > arr[i])
+            {
+                T temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
 }
